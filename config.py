36ea099bea4e5784f3dbfcd6a75b3c7ea3ef203a
entrepreneurim <iamakashrabha@gmail.com>
2025-05-04 10:45:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from typing import List
+
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "")
+
+IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
